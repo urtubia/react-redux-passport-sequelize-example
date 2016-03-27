f42e5a6fd482cf1dee130415697e267ad95a5c6d
Hector Urtubia <urtubia@gmail.com>
2016-03-27 14:33:59 -0400

    Add export of Signup page

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,6 +4,7 @@ export Home from './Home/Home';
 export Widgets from './Widgets/Widgets';
 export About from './About/About';
 export Login from './Login/Login';
+export Signup from './Signup/Signup';
 export LoginSuccess from './LoginSuccess/LoginSuccess';
 export Survey from './Survey/Survey';
 export NotFound from './NotFound/NotFound';
